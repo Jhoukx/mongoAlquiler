f4d8e303aa8cdba2f525ee5324599d169bfecba7
John <jegonzalezesparragoza@gmail.com>
2023-08-04 17:22:26 -0500

    feat: :sparkles: Post con rate limit

--- routes/campus.routes.js
+++ routes/campus.routes.js
@@ -11,7 +11,21 @@ appCampus.get('/',limitGet(),async(req,res)=>{
     let {id} = req.body; 
     let db = await con();
     let usuario = db.collection("usuario");
-    let result = await usuario.find({_id:new ObjectId(id)}).toArray();
+    let result = await usuario.find({},{_id:0}).toArray();
     res.send(result);
 });
+
+appCampus.post('/',async(req,res)=>{
+    if(!req.rateLimit) return;
+    let db = await con();
+    let usuario = db.collection("usuario");
+    try {
+        let result = await usuario.insertOne(req.body);
+        console.log(result);
+        res.send('Insert Successfully');
+    } catch (error) {
+        console.log(error);
+        res.send('Insert unsuccesfully')
+    }
+})
 export default appCampus;
