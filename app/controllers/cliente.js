71a38dee859631ba35d3ff03e0e134514bb6d5cb
John <jegonzalezesparragoza@gmail.com>
2023-08-15 17:54:21 -0500

    feat: :sparkles: Consulta para Mostrar todos los clientes registrados

--- app/controllers/cliente.js
+++ app/controllers/cliente.js
@@ -1,8 +1,14 @@
 import { con } from '../../config/connection/atlas.js';
 
+let db = await con();
+let cliente = db.collection("Cliente");
+
 const allclients = async (req, res) => {
     if (!req.rateLimit) return;
     console.log(req.rateLimit);
 
-    
+    let result = await cliente.find({}).toArray();
+    res.send(result);
 }
+
+export default allclients;
--- app/routes/cliente.routes.js
+++ app/routes/cliente.routes.js
@@ -1,18 +1,10 @@
 import { Router } from 'express';
-import { con } from '../../config/connection/atlas.js';
 import { limitGet } from '../middleware/limit.js';
+import allclients from '../controllers/cliente.js';
 
 const appCliente = Router();
 
 
-appCliente.get('/', limitGet(), async (req, res) => {
-    if (!req.rateLimit) return;
-    console.log(req.rateLimit);
-
-    let db = await con();
-    let cliente = db.collection("Cliente");
-    let result = await cliente.find({}).toArray();
-    res.send(result);
-});
+appCliente.get('/', limitGet(), allclients);
 
 export default appCliente;
